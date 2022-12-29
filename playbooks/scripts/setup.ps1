99739afef69dfc9093e20c1dfa93b8330ce566b8
xhiroga <13391129+xhiroga@users.noreply.github.com>
2022-12-29 12:16:13 +0900

    chore: enable remote desktop

--- playbooks/scripts/setup.ps1
+++ playbooks/scripts/setup.ps1
@@ -1,11 +1,13 @@
 # Setup Windows host for Ansible
 
 # Install OpenSSH Server
-# [OpenSSH をインストールする | Microsoft Learn](https://learn.microsoft.com/ja-jp/windows-server/administration/openssh/openssh_install_firstuse)
+## [OpenSSH をインストールする | Microsoft Learn](https://learn.microsoft.com/ja-jp/windows-server/administration/openssh/openssh_install_firstuse)
 Get-WindowsCapability -Online | Where-Object Name -like 'OpenSSH*'
 
 Start-Service sshd
 Set-Service -Name sshd -StartupType 'Automatic'
 
-# [windowsのsshのシェルをcmd.exeから ps/wsl bashへ切り替える。 - それマグで！](https://takuya-1st.hatenablog.jp/entry/2022/03/04/171043)
+## [windowsのsshのシェルをcmd.exeから ps/wsl bashへ切り替える。 - それマグで！](https://takuya-1st.hatenablog.jp/entry/2022/03/04/171043)
 New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
+
+# Enable Remote Desktop
